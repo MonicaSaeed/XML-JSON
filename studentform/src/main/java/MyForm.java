84cf8dd6870add504297eab7ce8d0560091f79ad
Monica Saeed <monicasaeed12@gmail.com>
2023-11-27 20:04:56 +0200

    Build an XML document, Give the user the ability to search ,and Choose any record to be deleted

---
+++ studentform/src/main/java/MyForm.java
@@ -0,0 +1,67 @@
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+
+// Url: http://localhost:8080/studentform/form
+public class MyForm extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        // Display the form to enter the number of students
+        out.println("<html><head><title>Student Form</title></head><body>");
+        out.println("<h2>Enter the number of students:</h2>");
+        out.println("<form method='post' action='form'>");
+        // to get the number of students from the user
+        out.println("<input type='number' name='numberOfStudents' required>");
+        out.println("<input type='submit' value='Next'>");
+        out.println("</form>");
+        out.println("</body></html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Get the number of students from the submitted form data
+        String numberOfStudentsString = request.getParameter("numberOfStudents");
+        int numberOfStudents = 0;
+        // Convert the string value to an int after checking for null
+        if (numberOfStudentsString != null && !numberOfStudentsString.isEmpty()) {
+            numberOfStudents = Integer.parseInt(numberOfStudentsString);
+        }
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        // Display the form with inputs for each student based on the entered number
+        out.println("<html><head><title>Student Form</title></head><body>");
+        out.println("<h2>Enter details for each student:</h2>");
+        out.println("<form method='post' action='submit-form'>");
+
+        // Add a hidden input to store the number of students to be retrieved later in
+        // the submit-form servlet
+        out.println("<input type='hidden' name='numberOfStudents' value='" + numberOfStudents + "'><br>");
+
+        // Add inputs for each student
+        for (int i = 1; i <= numberOfStudents; i++) {
+            out.println("<p>Student " + i + ":</p>");
+            // get input for id
+            out.println("ID: <input type='number' name='id" + i + "' required><br>");
+            out.println("First Name: <input type='text' name='firstname" + i + "' required><br>");
+            out.println("Last Name: <input type='text' name='lastname" + i + "' required><br>");
+            out.println("Gender: <input type='text' name='gender" + i + "' required><br>");
+            // get input for gpa with constraints <= 0 and >= 4 (float)
+            out.println("GPA: <input type='number' name='gpa" + i + "' min='0' max='4' step='0.01' required><br>");
+            // get input for level with constraints >= 1 and <= 4
+            out.println("Level: <input type='number' name='level" + i + "' min='1' max='4' required><br>");
+            out.println("Address: <input type='text' name='address" + i + "' required><br>");
+            out.println("");
+        }
+        out.println("<input type='submit' value='Submit'>");
+        out.println("</form>");
+        out.println("</body></html>");
+    }
+
+    // }
+}
---
+++ studentform/src/main/java/Search.java
@@ -0,0 +1,164 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+@WebServlet("/search")
+public class Search extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+
+        // Display the search form
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Search Form</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h2>Search Form</h2>");
+        out.println("<form action=\"search\" method=\"post\">");
+        out.println("<label for=\"searchType\">Search Type:</label>");
+        out.println("<select name=\"searchType\" id=\"searchType\">");
+        out.println("<option value=\"FirstName\">First Name</option>");
+        out.println("<option value=\"GPA\">GPA</option>");
+        out.println("</select>");
+        out.println("<br>");
+        out.println("<label for=\"searchTerm\">Search Term:</label>");
+        out.println("<input type=\"text\" name=\"searchTerm\" id=\"searchTerm\" required>");
+        out.println("<br>");
+        out.println("<input type=\"submit\" value=\"Search\">");
+        out.println("</form>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+
+        // Retrieve search parameters from the form
+        String searchType = request.getParameter("searchType");
+        String searchTerm = request.getParameter("searchTerm");
+
+        // Search the XML file based on the specified criteria
+        List<Student> searchResults = performSearch(searchType, searchTerm);
+
+        // Display search results
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Search Results</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h2>Search Results</h2>");
+
+        if (searchResults.isEmpty()) {
+            out.println("<p>No results found for the specified criteria.</p>");
+        } else {
+            out.println("<table border=\"1\">");
+            out.println("<tr>");
+            out.println("<th>ID</th>");
+            out.println("<th>First Name</th>");
+            out.println("<th>Last Name</th>");
+            out.println("<th>Gender</th>");
+            out.println("<th>GPA</th>");
+            out.println("<th>Level</th>");
+            out.println("<th>Address</th>");
+            out.println("</tr>");
+
+            for (Student student : searchResults) {
+                out.println("<tr>");
+                out.println("<td>" + student.ID + "</td>");
+                out.println("<td>" + student.firstName + "</td>");
+                out.println("<td>" + student.lastName + "</td>");
+                out.println("<td>" + student.gender + "</td>");
+                out.println("<td>" + student.GPA + "</td>");
+                out.println("<td>" + student.level + "</td>");
+                out.println("<td>" + student.address + "</td>");
+
+                // Add a delete button for each row
+                out.println("<td>");
+                out.println("<form action=\"delete\" method=\"post\">");
+                // hidden input to store the ID of the student to delete
+                out.println("<input type=\"hidden\" name=\"id\" value=\"" + student.ID + "\">");
+                out.println("<input type=\"submit\" value=\"Delete\">");
+                out.println("</form>");
+                out.println("</td>");
+
+                out.println("</tr>");
+            }
+
+            out.println("</table>");
+        }
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    private List<Student> performSearch(String searchType, String searchTerm) {
+        List<Student> searchResults = new ArrayList<>();
+
+        try {
+            // Load the XML file
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse("data/Students.xml");
+
+            // Normalize the XML structure
+            doc.getDocumentElement().normalize();
+
+            // Get the root element
+            Element rootElement = doc.getDocumentElement();
+
+            // Get all student nodes
+            NodeList studentNodes = rootElement.getElementsByTagName("Student");
+
+            // Iterate through the student nodes
+            for (int i = 0; i < studentNodes.getLength(); i++) {
+                Node studentNode = studentNodes.item(i);
+
+                if (studentNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element studentElement = (Element) studentNode;
+
+                    // Retrieve values from XML
+                    String firstName = studentElement.getElementsByTagName("firstName").item(0).getTextContent();
+                    String gpa = studentElement.getElementsByTagName("gpa").item(0).getTextContent();
+
+                    // Perform the search based on the specified criteria
+                    if (("FirstName".equalsIgnoreCase(searchType) && firstName.contains(searchTerm))
+                            || ("GPA".equalsIgnoreCase(searchType) && gpa.equals(searchTerm))) {
+                        // If the search criteria match, create a Student object and add it to the
+                        // results
+                        String id = studentElement.getAttribute("ID");
+                        String lastName = studentElement.getElementsByTagName("lastName").item(0).getTextContent();
+                        String gender = studentElement.getElementsByTagName("Gender").item(0).getTextContent();
+                        String level = studentElement.getElementsByTagName("level").item(0).getTextContent();
+                        String address = studentElement.getElementsByTagName("address").item(0).getTextContent();
+
+                        Student student = new Student(id, firstName, lastName, gender, gpa, level, address);
+                        searchResults.add(student);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return searchResults;
+    }
+}
